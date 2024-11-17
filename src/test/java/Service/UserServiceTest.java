a50a310bd115103c28774191e5c9b6e4ce310c77
Nickson <nnkipkorir@dtbafrica.com>
2024-11-17 11:18:04 +0300

    use mockito to mock objects , stab data

--- src/test/java/Service/UserServiceTest.java
+++ src/test/java/Service/UserServiceTest.java
@@ -1,10 +1,16 @@
 package Service;
 
+import data.UserRepository;
 import model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static Utility.Constants.empty_first_name;
 import static Utility.Constants.empty_last_name;
@@ -15,9 +21,17 @@ import static org.junit.jupiter.api.Assertions.*;
  * created 15/11/2024
  */
 
+
+//todo: configure mockito to allow us use mockito annotations
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
     //create member variables here for reusability
-    UserService userService;
+    //todo: to make mockito inject mocks for us we will need to use the service and not the interface nd use @InjectMocks annotation
+    @InjectMocks
+    UserServiceImpl userService;
+    @Mock
+    UserRepository userRepository;
+    //todo : tell mockito what to do when we invoke methods in this interface
     String firstName;
     String lastName;
     String email;
@@ -26,7 +40,7 @@ public class UserServiceTest {
 
     @BeforeEach
     void setUp() {
-        userService = new UserServiceImpl();
+       // userService = new UserServiceImpl(userRepository); // inject the mocked repository
         firstName = "Nickson";
         lastName = "Brown";
         email = "nickson.brown@gmail.com";
@@ -37,6 +51,9 @@ public class UserServiceTest {
     @Test
     @DisplayName("User object created")
     void testCreateUser_whenUserDetailsProvided_returnsUserObject() {
+        //arrange
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(true); //save method requires a parameter, we are not calling a real save method so what we pass does not really matter
+                                                                // hence we stab the data using mockito arguments matcher Mockito.any(Class)
         //act
         User user = userService.createUser(firstName, lastName, email , password , repeatPassword);  // create the test method in the UserService so as to continue
         //assert
